8d981702517aeba600467284c05149cfd172a5d3
gulceTuncel <gulcetuncell@gmail.com>
2024-08-18 15:48:18 +0300

    new challenge solutions added

---
+++ LeetCode75/GreatestCommonDivisorOfStrings/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        // Example 1
+        Console.WriteLine(solution.GcdOfStrings("ABCABC", "ABC")); // Output: "ABC"
+
+        // Example 2
+        Console.WriteLine(solution.GcdOfStrings("ABABAB", "ABAB")); // Output: "AB"
+
+        // Example 3
+        Console.WriteLine(solution.GcdOfStrings("LEET", "CODE")); // Output: ""
+    }
+
+    public string GcdOfStrings(string str1, string str2)
+    {
+        int len1 = str1.Length;
+        int len2 = str2.Length;
+
+        int gcdLength = GCD(len1, len2);
+
+        string candidate = str1.Substring(0, gcdLength);
+
+        if (IsDivisible(str1, candidate) && IsDivisible(str2, candidate))
+        {
+            return candidate;
+        }
+
+        return "";
+    }
+
+    private int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    private bool IsDivisible(string str, string x)
+    {
+        int lenX = x.Length;
+        int lenStr = str.Length;
+
+        if (lenStr % lenX != 0)
+            return false;
+
+        string repeatedX = new string(x[0], lenStr / lenX).Replace(x[0].ToString(), x);
+
+        return repeatedX == str;
+    }
+}
---
+++ LeetCode75/KidsWithTheGreatestNumberOfCandies/Program.cs
@@ -0,0 +1,51 @@
+﻿public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        // Example 1
+        int[] candies1 = { 2, 3, 5, 1, 3 };
+        int extraCandies1 = 3;
+        Console.WriteLine(string.Join(",", solution.KidsWithCandies(candies1, extraCandies1))); // Output: true,true,true,false,true
+
+        // Example 2
+        int[] candies2 = { 4, 2, 1, 1, 2 };
+        int extraCandies2 = 1;
+        Console.WriteLine(string.Join(",", solution.KidsWithCandies(candies2, extraCandies2))); // Output: true,false,false,false,false
+
+        // Example 3
+        int[] candies3 = { 12, 1, 12 };
+        int extraCandies3 = 10;
+        Console.WriteLine(string.Join(",", solution.KidsWithCandies(candies3, extraCandies3))); // Output: true,false,true
+    }
+
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        int maxCandies = 0;
+
+        foreach (int candy in candies)
+        {
+            if (candy > maxCandies)
+            {
+                maxCandies = candy;
+            }
+
+        }
+
+        IList<bool> result = new List<bool>();
+        foreach (int candy in candies)
+        {
+            if (candy + extraCandies >= maxCandies)
+            {
+                result.Add(true);
+            }
+            else
+            {
+                result.Add(false);
+            }
+        }
+
+        return result;
+    }
+}
